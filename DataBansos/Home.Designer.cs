fd8a39f22208c8e5c972bcf591354e38c0298950
BimaHuberta19 <120377572+BimaHuberta19@users.noreply.github.com>
2023-01-04 00:56:30 +0700

    membuat from home

--- DataBansos/Home.Designer.cs
+++ DataBansos/Home.Designer.cs
@@ -29,34 +29,150 @@ namespace DataBansos
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.pnlContent = new System.Windows.Forms.Panel();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.lblSelected2 = new System.Windows.Forms.Label();
+            this.btnReporting = new System.Windows.Forms.Button();
+            this.lblSelected1 = new System.Windows.Forms.Label();
+            this.btnInputanData = new System.Windows.Forms.Button();
+            this.btntentangaplikasi = new System.Windows.Forms.Button();
+            this.pnlLeft = new System.Windows.Forms.Panel();
+            this.Panel1.SuspendLayout();
+            this.pnlLeft.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // pnlContent
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(231, 141);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 30);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Selamat Datang";
+            this.pnlContent.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContent.Location = new System.Drawing.Point(276, 0);
+            this.pnlContent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(861, 619);
+            this.pnlContent.TabIndex = 8;
+            // 
+            // Panel1
+            // 
+            this.Panel1.Controls.Add(this.Button4);
+            this.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Panel1.Location = new System.Drawing.Point(0, 559);
+            this.Panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(276, 60);
+            this.Panel1.TabIndex = 6;
+            // 
+            // Button4
+            // 
+            this.Button4.FlatAppearance.BorderSize = 0;
+            this.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Button4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Button4.Location = new System.Drawing.Point(0, 1);
+            this.Button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(276, 57);
+            this.Button4.TabIndex = 7;
+            this.Button4.Text = "LogOut";
+            this.Button4.UseVisualStyleBackColor = true;
+            // 
+            // lblSelected2
+            // 
+            this.lblSelected2.BackColor = System.Drawing.Color.White;
+            this.lblSelected2.Location = new System.Drawing.Point(-1, 375);
+            this.lblSelected2.Name = "lblSelected2";
+            this.lblSelected2.Size = new System.Drawing.Size(12, 39);
+            this.lblSelected2.TabIndex = 5;
+            this.lblSelected2.Visible = false;
+            // 
+            // btnReporting
+            // 
+            this.btnReporting.FlatAppearance.BorderSize = 0;
+            this.btnReporting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReporting.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnReporting.Location = new System.Drawing.Point(3, 375);
+            this.btnReporting.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReporting.Name = "btnReporting";
+            this.btnReporting.Size = new System.Drawing.Size(276, 39);
+            this.btnReporting.TabIndex = 4;
+            this.btnReporting.Text = "Data Reporting";
+            this.btnReporting.UseVisualStyleBackColor = true;
+            // 
+            // lblSelected1
+            // 
+            this.lblSelected1.BackColor = System.Drawing.Color.White;
+            this.lblSelected1.Location = new System.Drawing.Point(-3, 315);
+            this.lblSelected1.Name = "lblSelected1";
+            this.lblSelected1.Size = new System.Drawing.Size(12, 39);
+            this.lblSelected1.TabIndex = 3;
+            this.lblSelected1.Visible = false;
+            // 
+            // btnInputanData
+            // 
+            this.btnInputanData.FlatAppearance.BorderSize = 0;
+            this.btnInputanData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInputanData.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnInputanData.Location = new System.Drawing.Point(1, 315);
+            this.btnInputanData.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnInputanData.Name = "btnInputanData";
+            this.btnInputanData.Size = new System.Drawing.Size(276, 39);
+            this.btnInputanData.TabIndex = 2;
+            this.btnInputanData.Text = "Inputan Data ";
+            this.btnInputanData.UseVisualStyleBackColor = true;
+            // 
+            // btntentangaplikasi
+            // 
+            this.btntentangaplikasi.FlatAppearance.BorderSize = 0;
+            this.btntentangaplikasi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btntentangaplikasi.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btntentangaplikasi.Location = new System.Drawing.Point(0, 258);
+            this.btntentangaplikasi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btntentangaplikasi.Name = "btntentangaplikasi";
+            this.btntentangaplikasi.Size = new System.Drawing.Size(276, 39);
+            this.btntentangaplikasi.TabIndex = 0;
+            this.btntentangaplikasi.Text = "Tentang Aplikasi";
+            this.btntentangaplikasi.UseVisualStyleBackColor = true;
+            // 
+            // pnlLeft
+            // 
+            this.pnlLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(117)))), ((int)(((byte)(136)))));
+            this.pnlLeft.Controls.Add(this.Panel1);
+            this.pnlLeft.Controls.Add(this.lblSelected2);
+            this.pnlLeft.Controls.Add(this.btnReporting);
+            this.pnlLeft.Controls.Add(this.lblSelected1);
+            this.pnlLeft.Controls.Add(this.btnInputanData);
+            this.pnlLeft.Controls.Add(this.btntentangaplikasi);
+            this.pnlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLeft.Location = new System.Drawing.Point(0, 0);
+            this.pnlLeft.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlLeft.Name = "pnlLeft";
+            this.pnlLeft.Size = new System.Drawing.Size(276, 619);
+            this.pnlLeft.TabIndex = 6;
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1137, 619);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.pnlLeft);
             this.Name = "Home";
             this.Text = "Home";
+            this.Panel1.ResumeLayout(false);
+            this.pnlLeft.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Panel pnlContent;
+        internal System.Windows.Forms.Panel Panel1;
+        internal System.Windows.Forms.Button Button4;
+        internal System.Windows.Forms.Label lblSelected2;
+        internal System.Windows.Forms.Button btnReporting;
+        internal System.Windows.Forms.Label lblSelected1;
+        internal System.Windows.Forms.Button btnInputanData;
+        internal System.Windows.Forms.Button btntentangaplikasi;
+        internal System.Windows.Forms.Panel pnlLeft;
     }
 }
